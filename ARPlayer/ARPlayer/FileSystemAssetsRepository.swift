2299214a718b2fef1a586a0387ea45b69be7f15e
Tiago <tiago@MacBook-Pro-de-Tiago.local>
2018-06-21 17:16:24 +0200

    Use safe try instead of do catch

--- ARPlayer/ARPlayer/FileSystemAssetsRepository.swift
+++ ARPlayer/ARPlayer/FileSystemAssetsRepository.swift
@@ -10,13 +10,8 @@ class FileSystemAssetsRepository: AssetsRepository {
     
     func save(from URL: URL) {
         guard let userDocumentDirectory = fileManager.userDocumentDirectory else { return }
-        do {
-            let finalDirectory = userDocumentDirectory.appendingPathComponent(URL.lastPathComponent)
-            try fileManager.removeItem(at: finalDirectory)
-            try fileManager.moveItem(at: URL, to: finalDirectory)
-            print("Moved")
-        } catch let error {
-            print("Error moving \(error)")
-        }
+        let finalDirectory = userDocumentDirectory.appendingPathComponent(URL.lastPathComponent)
+        try? fileManager.removeItem(at: finalDirectory)
+        try? fileManager.moveItem(at: URL, to: finalDirectory)
     }
 }
---
+++ Scratchpad.md
@@ -0,0 +1,4 @@
+- Ask user if he wants to overwrite or keep both files
+= Handle error in save and delete in filesystem
+
+- Read current assets from repository
